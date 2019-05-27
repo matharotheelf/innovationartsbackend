f932da5b5e2d69c6f252f3a7aca7b198a59f755d
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-05-27 20:52:14 +0100

    Added route which gets json from s3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import boto3
 from app import app
+import json
 
 @app.route("/posttos3", methods=['POST'])
 def posttos3():
@@ -8,3 +9,10 @@ def posttos3():
     object = s3.Object('innovationarts', 'json.txt')
     object.put(Body=json)
     return "done"
+
+@app.route("/")
+def getfroms3():
+    s3 = boto3.resource('s3')
+    content_object = s3.Object('innovationarts', 'json.txt')
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    return file_content
