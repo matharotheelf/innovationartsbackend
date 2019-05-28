0874ab67005864531b589e8958a6a095f179026f
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-05-28 17:16:42 +0100

    Refactor: dried get from s3 route

--- app/routes.py
+++ app/routes.py
@@ -14,5 +14,4 @@ def posttos3():
 def getfroms3():
     s3 = boto3.resource('s3')
     content_object = s3.Object('innovationarts', 'json.txt')
-    file_content = content_object.get()['Body'].read().decode('utf-8')
-    return file_content
+    return content_object.get()['Body'].read().decode('utf-8')
